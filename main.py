740a1118e4f2bbab9f25fd793298e504c685f874
Hans Arevalo <60830264+hansArevalo@users.noreply.github.com>
2022-06-22 19:16:45 +0000

    bias o sesgo

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
+import numpy as np
+#creamos la clase pereceptron
+class Perceptron():
+  def __init__(self,inputs,weights):
+    self.inputs=np.array(inputs)
+    self.weights=np.array(inputs)
+ 
 
-print(preguntas)
+  def decide(self,treshold):
+    total=0
+    
+    for input_,weight_ in zip(self.inputs,self.weights):
+      total+=input_*weight_  
+    if total>=treshold:
+      valor=True
+    else:
+      valor=False
+    print(valor)
+    return valor
+inputs,weights=[],[]
+
+preguntas=[
+  "¿cual es la velocidad?.. ",
+  "¿ritmo cardiaco?.. ",
+  "respiracion.. "
+]
+
+for pregunta in preguntas:
+  i=int(input(pregunta))
+  w=int(input("y sus peso asociado es... "))
+  inputs.append(i)
+  weights.append(w)
+  print()
+treshold=int(input("y nuestro umbral/limite sera: ")) 
+
+
+p=Perceptron(inputs,weights)
+print(p.decide(treshold))
